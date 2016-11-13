d5f5d052321ecaabf47cb819620d22031aac6073
Tom Lyons <tbl3rd@gmail.com>
2016-11-13 11:25:54 -0500

    Remove obsolete files.

--- src/church.clj
+++
@@ -1,5 +0,0 @@
-(def T (fn [t e] t))                    ; true
-(def F (fn [t e] e))                    ; false
-(def IF (fn [b t e] (b t e)))
-(IF T "true" "false")                   ;-=> "true"
-(IF F "true" "false")                   ;-=> "false"
