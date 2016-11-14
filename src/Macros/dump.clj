50472aa0e7f0700c516e41db06aaa5d90d0319ae
Tom Lyons <tbl3rd@gmail.com>
2016-11-13 21:35:04 -0500

    First cut at binding and macros.

---
+++ src/Macros/dump.clj
@@ -0,0 +1,111 @@
+;;
+;; a DUMP macro
+;;
+
+;; We want something to use when debugging such that the content of
+;; this comment ...
+
+(def beatle? #{:george :john :paul :ringo})
+
+(comment
+  (dump beatle?)
+  (dump (count beatle?))
+  (dump (beatle? :ringo))
+  (dump (beatle? :mick)))
+
+;; ... prints the lines in this comment.
+
+(comment
+  [beatle? #{:george :john :paul :ringo}]
+  [(count beatle?) 4]
+  [(beatle? :ringo) true]
+  [(beatle? :mick) false])
+
+;; DUMP cannot be defined as a function.  Why?
+
+;; DUMP can be defined as a macro that turns this ...
+
+(comment
+  (dump beatle?)
+  (dump (count beatle?))
+  (dump (beatle? :ringo))
+  (dump (beatle? :mick)))
+
+;; ... into this.
+
+(comment
+  (println [(quote beatle?) beatle?])
+  (println [(quote (count beatle?)) (count beatle?)])
+  (println [(quote (beatle? :ringo)) (beatle? :ringo)])
+  (println [(quote (beatle? :mick)) (beatle? :mick)])
+  )
+
+;; What if we had a function that produced the list we want from the
+;; list we have?
+
+(defn dump-fn
+  [expression]
+  (list 'println [(list 'quote expression) expression]))
+
+(dump-fn (quote beatle?))     ;-=> (println [(quote beatle?) beatle?])
+(dump-fn (quote (count beatle?)))
+
+;; Given DUMP-FAN we can define a macro that transforms the expression
+;; we have into the expression we want.
+
+(defmacro dump
+  [expression]
+  (dump-fn expression))
+
+;; The DUMP macro just passes its EXPRESSION unevaluated to DUMP-FN,
+;; which then turns the incoming expression into the PRINTLN form,
+;; which Clojure then evaluates.
+
+(dump beatle?)
+(dump (count beatle?))
+
+;; The MACROEXPAND function takes a quoted expression and returns the
+;; resulting expression that Clojure evaluates after expanding macros.
+
+(macroexpand '(dump beatle?))
+;;-=> (clojure.core/println [(quote beatle?) beatle?])
+
+(macroexpand '(dump (count beatle?)))
+;;-=> (clojure.core/println [(quote (count beatle?)) (count beatle?)])
+
+;; The construction of new list forms from old list forms is such a
+;; common thing when writing macros, that Clojure introduces some more
+;; shorthand quoting forms: the QUASIQUOTE ` (backtick) and UNQUOTE ~
+;; (tilda).  You can use them to construct a list template that the
+;; macro fills in.
+
+;; QUASIQUOTE is like QUOTE except that inside a QUASIQUOTEd form, an
+;; UNQUOTEd symbol is replaced with the expression bound to it.
+
+(let [expression 'beatles?]
+  `(println [(quote ~expression) ~expression]))
+;;-=> (clojure.core/println [(quote beatles?) beatles?])
+
+;; You can think of QUASIQUOTE as introducing a template into which
+;; new expressions can be injected with UNQUOTE.
+
+(defmacro dump
+  [expression]
+  `(println [(quote ~expression) ~expression]))
+
+;; Unlike QUOTE, you don't actually need QUASIQUOTE and UNQUOTE to
+;; write macros, but you will see them used by others, and they can
+;; streamline macros by removing the need for helper functions like
+;; DUMP-FN.
+
+(dump beatle?)
+(dump (beatle? :ringo))
+(dump (if (beatle? :ringo) "Found!" "Nope."))
+(dump (if (beatle? :mick) "Found!" "Nope."))
+
+;; There is another UNQUOTE form called SPLICE, which adds an unquoted
+;; form to the expression surrounding it.
+
+(defn do-or-nil-fn
+  [& stuff]
+  )
---
+++ src/o-bind.clj
@@ -0,0 +1,105 @@
+;;
+;; Binding forms
+;;
+
+;; FN, LET, and DEF are known as BINDING forms because they bind
+;; names (represented as symbols on the left) to values evaluated from
+;; expressions on the right.
+
+;; So far, the left side of all our bindings have been simple symbols,
+;; but Clojure also allows vector and map expressions on the left side
+;; as the targets of bindings.  Vectors bind sequence values (such as
+;; vectors, lists and sequences) and maps bind associative values such
+;; as maps and sets.
+
+;; Here is a SWAP function, for example.
+
+(defn swap [[x y]] [y x])               ; Bind a vector of 2 elements.
+(swap [:left :right])                   ;-=> [:right :left]
+
+;; Applying SWAP to [:left :right] binds X to :left and Y to :right.
+;; Then the resulting vector swaps X and Y, swapping the values.
+;; But any value can be bound this way.
+
+(swap [[0 1] [2 3]])                    ;-=> [[2 3] [0 1]]
+(swap ["left" {:a 0 :b 1}])             ;-=> [{:a 0, :b 1} "left"]
+
+;; Bindings can nest.
+
+(defn swamp
+  [[a b] [x y]]
+  [[y x] [b a]])
+
+(swamp [0 1] [2 3])                     ;-=> [[3 2] [1 0]]
+
+;; The keyword :as binds its following form to the original
+;; expression's value.
+
+(defn show-rotate
+  [[a b c :as was]]
+  [:was was :now [c a b]])
+
+(show-rotate [0 1 2])                 ;-=> [:was [0 1 2] :now [2 0 1]]
+
+;; A naked ampersand & is special in binding forms too.  It means bind
+;; the values of all following expressions to the next expression as a
+;; sequence.
+
+(defn show-rotate-more
+  [[a b c & more :as was]]
+  [:more more :was was :now [c a b]])
+
+(show-rotate-more [0 1 2 3 4])
+;; -=> [:more (3 4) :was [0 1 2 3 4] :now [2 0 1]]
+
+;; It is not unusual to see a variable number of arguments bound to a
+;; single parameter as a list using &.  The most common case is MAIN.
+
+(defn main [& args]
+  [:args args])
+
+(main "echo" "-n" "No" "newline" "here.")
+;;-=> [:args ("echo" "-n" "No" "newline" "here.")]
+
+(defn really-rotate
+  "Bind ARGS to a SEQuence of all the arguments."
+  [& args]
+  (vec (cons (last args) (butlast args))))
+
+(really-rotate 0 1 2 3 4)               ;-=> [4 0 1 2 3]
+
+;; A map binding behaves kind of like keyword arguments in languages
+;; like Python.
+
+(defn ->person
+  [{name :name birth :birth}]
+  {:kind :person :name name :birth (or birth :unknownn)})
+
+(->person {:name "Tom"})
+;;-=> {:kind :person, :name "Tom", :birth :unknownn}
+
+;; You can even supply default arguments value with :or.
+
+(defn ->person
+  [{name :name birth :birth :or {birth :unknown}}]
+  {:kind :person :name name :birth birth})
+
+(->person {:name "Tom"})
+;;-=> {:kind :person, :name "Tom", :birth :unknownn}
+
+;; But again, as often in Clojure, the {foo :foo bar :bar} pattern is
+;; so common that there is a simpler way to write it with a keyword.
+
+(defn ->person
+  [{:keys [name birth]}]
+  {:kind :person :name name :birth (or birth :unknownn)})
+
+(->person {:name "Tom"})
+
+;; There are also the much less common :syms and :strs keywords in map
+;; bindings, which bind map and set values whose keys are symbols or
+;; strings.
+
+;; Destructuring is implemented in FN, so it is available in all the
+;; binding forms: FN, LET, DEF, DEFN, DEFMACRO, BINDING, and so on --
+;; even binding forms you might write yourself.
